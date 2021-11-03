dfb132367b26e1d04cb6f765847ce7d8e70d9bb3
GenevieveFederi <88980301+GenevieveFederico@users.noreply.github.com>
2021-11-03 15:28:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# a121_catch_a_turtle.py
+#-----import statements-----
+import turtle as trtl
+import random as rand
+#-----game configuration----
+spotColor = "pink"
+spotShape = "circle"
+spotSize = 2
+
+#-----initialize turtle-----
+spot = trtl.Turtle()
+spot.shape(spotShape)
+spot.turtlesize(spotSize)
+spot.fillcolor(spotColor)
+spot.penup()
+#-----game functions--------
+def spot_clicked(x,y):
+  print("Spot was clicked!")
+  change_position()
+
+def change_position():
+  new_xpos = rand.randint(-180,180)
+  new_ypos = rand.randint(-140,140)
+  spot.hideturtle()
+  spot.goto(new_xpos,new_ypos)
+  spot.showturtle()
+#-----events----------------
+spot.onclick(spot_clicked)
+wn = trtl.Screen()
+wn.mainloop()
